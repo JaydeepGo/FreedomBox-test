4440f2f2d13c84cd71d899bf80315ede0c2c0875
jd147 <jaydeep415@gmail.com>
2018-03-27 18:38:54 +0530

    Add files via upload
    tower of hanoi bash script

---
+++ towerofhanoi.sh
@@ -0,0 +1,26 @@
+#Tower of hanoi problem with the help of Bash script
+# hanoi(number of disks, source tower, destination tower, temporary tower)
+#!/bin/bash
+function hanoi
+{
+  if [ $1 -eq 0 ]
+  then
+    return 0;
+  else
+    hanoi $(($1-1)) $2 $4 $3
+    echo "Move disk $1 from tower $2 to tower $3"
+    hanoi $(($1-1)) $4 $3 $2
+  fi
+}
+
+
+echo "Enter the number of disk in tower: "
+read totaldisk 
+
+if ! [[ $totaldisk =~ ^[0-9]+$ ]]  
+then
+  echo "Input interger only"
+  exit;
+else
+  hanoi $totaldisk A B C
+fi
