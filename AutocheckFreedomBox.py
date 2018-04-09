809fcda9dabb48f82bd79e58e6c8bd1ba7fe6834
Jaydeep Godara <jaydeep415@gmail.com>
2018-04-09 18:47:16 +0530

    fix conditions

--- AutocheckFreedomBox.py
+++ AutocheckFreedomBox.py
@@ -3,28 +3,31 @@ import requests
 import bs4
 
 
-print("Enter ip address of freedombox with https:// and quotes ")
+print "Enter ip address of freedombox with https:// and quotes "
 freedombox_ip=input() 
 
-site = requests.get(freedombox_ip, verify=False)
+site = requests.get(freedombox_ip)
+#use this if get certificate error 'site = requests.get(freedombox_ip, verify=False)'
+
 response_code = site.status_code
 
 Sdata = bs4.BeautifulSoup(site.text, "lxml")
 Stitle_big = Sdata.title.text
 Stitle = Stitle_big.strip()
 
-print ("############################################################################\n")
-print (response_code)
+print "############################################################################\n"
+print "Response code:", response_code
 
 
 if response_code == 200:
 	if Stitle  == "FreedomBox":
-		print("Freedombox Working fine")
+		print "Freedombox Working fine"
 	else:
-		print("Check IP of FreedomBox ")
+		print "Check IP of FreedomBox "
+
 elif response_code == 503:
-	print("Plinth is not running")
+	print "Plinth is not running"
 
 else :
-	print("Something Went Wrong : Install FreedomBox in proper way")
+	print "Something Went Wrong : Install FreedomBox in proper way"
  
